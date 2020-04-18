71478a0a5ef9f5eb45ac4013725652d29a28fd74
SnakesRCute <57366452+SnakesRCute@users.noreply.github.com>
2020-04-17 20:51:53 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,13 +2,15 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashMap;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class Board {
 	private ArrayList<PowerPlant> deck;
 	private ArrayList<PowerPlant> marketPlants;
-	private LinkedHashMap<String, Integer> resourceMarket;
+	private TreeMap<String, Integer> resourceMarket;//used to be LinkedHashMap
 	private Graph graph;
 	private int step;
 	public Board() throws IOException
@@ -16,7 +18,11 @@ public class Board {
 		deck = new ArrayList<PowerPlant>();
 		readPlantFile();
 		marketPlants = new ArrayList<PowerPlant>();
-		resourceMarket = new LinkedHashMap<String, Integer>();
+		resourceMarket = new TreeMap<String, Integer>();
+		resourceMarket.put("coal", 24);
+		resourceMarket.put("oil", 18);
+		resourceMarket.put("garbage", 6);
+		resourceMarket.put("uranium", 2);
 		graph = new Graph();
 		//havent seen the graph class yet so will edit later
 		step = 1;
@@ -34,7 +40,11 @@ public class Board {
 	{
 		return deck;
 	}
-	public LinkedHashMap<String, Integer> getResourceMarket() {
+	public void updateStep(int newStep)
+	{
+		step = newStep;
+	}
+	public TreeMap<String, Integer> getResourceMarket() {
 		return resourceMarket;
 	}
 	public ArrayList<PowerPlant> getMarketPlants() {
@@ -43,9 +53,65 @@ public class Board {
 	public Graph getGraph() {
 		return graph;
 	}
-	public void updateResourceMarket()
+	public void updateResourceMarket(Player[] players)
 	{
-		//TODO i think this needs a text file
+		TreeMap<String, Integer> remain = new TreeMap<String, Integer>();
+		remain.put("coal", 24);
+		remain.put("oil", 24);
+		remain.put("garbage", 24);
+		remain.put("uranium", 12);
+		for(Player p : players)
+		{
+			ArrayList<PowerPlant> plant = p.getPlants();
+			for(int i = 0; i < plant.size(); i++)
+			{
+				if(plant.get(i).getType().equals("hybrid"))
+				{
+					remain.put("coal", remain.get("coal")-plant.get(i).getNumCoal());
+					remain.put("oil", remain.get("oil")-plant.get(i).getNumOil());
+				}
+				else if(!plant.get(i).getType().equals("wind"))
+				{
+					remain.put(plant.get(i).getType(), remain.get(plant.get(i).getType())-plant.get(i).getMats());
+				}
+			}
+		}
+		Set<String> types = resourceMarket.keySet();
+		Iterator<String> itr = types.iterator();
+		while(itr.hasNext())
+		{
+			String type = itr.next();
+			remain.put(type, remain.get(type) - resourceMarket.get(type));
+		}
+		if(step == 1)
+		{
+			updateResourceMarket2(remain, "coal", 5);
+			updateResourceMarket2(remain, "oil", 3);
+			updateResourceMarket2(remain, "garbage", 2);
+			updateResourceMarket2(remain, "uranium", 1);
+		}
+		else if(step == 2)
+		{
+			updateResourceMarket2(remain, "coal", 6);
+			updateResourceMarket2(remain, "oil", 4);
+			updateResourceMarket2(remain, "garbage", 3);
+			updateResourceMarket2(remain, "uranium", 2);
+		}
+		else if(step == 3)
+		{
+			updateResourceMarket2(remain, "coal", 4);
+			updateResourceMarket2(remain, "oil", 5);
+			updateResourceMarket2(remain, "garbage", 4);
+			updateResourceMarket2(remain, "uranium", 2);
+		}
+		
+	}
+	public void updateResourceMarket2(TreeMap<String, Integer> remain, String type, int amt)
+	{
+		int toAdd = amt;
+		if(remain.get(type) < amt)
+			toAdd = remain.get(type);
+		resourceMarket.put(type, resourceMarket.get(type) + toAdd);
 	}
 	public ArrayList<Player> determineOrder(ArrayList<Player> players)
 	{
